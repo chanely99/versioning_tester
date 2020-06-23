806b5ff1512054e11f5dfe440e7dd877ad7a12da
Chanel Young <chanelyoung99@gmail.com>
2020-06-23 13:09:57 -0700

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Getting current version
-version=$(git describe --abbrev=0 --tags)
-version=${version:1} # Remove the v in the tag v0.37.10 for example
-IFS='.' read -r -a array <<< $version
-arr=($array)
-echo "$arr"
